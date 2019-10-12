be991a051d0784742f63523bf6d428cd402bcb17
cdk <keller.cameron@gmail.com>
2019-10-12 18:02:38 +1100

    Updates

--- code/src/systems/controllers/AIControllerSystem.lua
+++ code/src/systems/controllers/AIControllerSystem.lua
@@ -18,11 +18,19 @@ function AIControllerSystem:update(dt)
             for _,weapon in pairs(value.children) do
               weapon_component = weapon:get("Weapon") 
               if weapon_component ~= nil then
-                grid_item = weapon:get("GridItem")
+                local grid_item = weapon:get("GridItem")
+                local target_grid_range = AIController.target_range 
                 for _,target_grid in pairs(target.children) do
                   target_grid_item = target_grid:get("GridItem")
-                  grid_item.t_pos_grid_physics = math.atan2(target_grid_item.y_pos_grid_physics - grid_item.y_pos_grid_physics, target_grid_item.x_pos_grid_physics - grid_item.x_pos_grid_physics) + 1.5708
-                  grid_item.t_render = grid_item.t_pos_grid_physics - AIPhysics.body:getAngle()
+                  local t_pos_grid_physics = math.atan2(target_grid_item.y_pos_grid_physics - grid_item.y_pos_grid_physics, target_grid_item.x_pos_grid_physics - grid_item.x_pos_grid_physics) + 1.5708
+                  local t_render = t_pos_grid_physics - AIPhysics.body:getAngle()
+
+                  local target_grid_range_temp = math.sqrt((target_grid_item.y_pos_grid_physics - AIPhysics.body:getY())^2 + (target_grid_item.x_pos_grid_physics - AIPhysics.body:getX())^2)
+                  if target_grid_range_temp < target_grid_range then
+                    target_grid_range = target_grid_range_temp
+                    grid_item.t_pos_grid_physics = t_pos_grid_physics
+                    grid_item.t_render = t_render
+                  end
                 end
                 eventmanager:fireEvent(FireEvent(weapon, dt))
               end
@@ -34,9 +42,9 @@ function AIControllerSystem:update(dt)
       -- Really dumb way to turn towards enemy
       if AIController.target_direction ~= AIPhysics.body:getAngle() then
         if AIController.target_direction > AIPhysics.body:getAngle() then
-          AIPhysics.body:applyTorque(300000)
+          AIPhysics.body:applyTorque(100000)
         elseif AIController.target_direction < AIPhysics.body:getAngle() then
-          AIPhysics.body:applyTorque(-300000)
+          AIPhysics.body:applyTorque(-100000)
         end
       end
 
@@ -44,7 +52,7 @@ function AIControllerSystem:update(dt)
       if AIController.target_range > AIController.range then
         local magnitude_x = math.sin(AIPhysics.body:getAngle())
         local magnitude_y = math.cos(AIPhysics.body:getAngle())*-1
-        local force = 10000
+        local force = 3000
         AIPhysics.body:applyForce(magnitude_x * force, magnitude_y * force)
       end
     end
