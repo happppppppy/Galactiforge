6d601d2ea498ff4d6c5f39b450443e085098eea8
cdk <keller.cameron@gmail.com>
2019-10-12 18:36:24 +1100

    Implemented States

---
+++ code/src/states/MenuState.lua
@@ -0,0 +1,12 @@
+local MenuState = {}
+
+function MenuState:init()
+end
+
+function MenuState:update(dt)
+end
+
+function MenuState:draw()
+end
+
+return MenuState
---
+++ code/src/states/SplashState.lua
@@ -0,0 +1,19 @@
+local splashy = require("code/lib/splashy")
+
+local SplashState = {}
+
+function SplashState:init()
+  splashy.addSplash(love.graphics.newImage("assets/images/SplashScreen.png"))
+  splashy.onComplete(function() Gamestate.switch(MainState) end)
+
+end
+
+function SplashState:update(dt)
+  splashy.update(dt)
+end
+
+function SplashState:draw()
+  splashy.draw()
+end
+
+return SplashState
